d3abd9d46aff22c46b5c0008dad025ae68311838
alexandre.durieux <durieuxalexandre26@gmail.com>
2017-10-20 10:07:56 +0200

    //List command
    Add the "//list" command
    Display a page of the list of the emojis the bot have
    USAGE : //list [page]

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const Discord = require("discord.js"); //Bibliothèque Javascript de Discord
+ï»¿const Discord = require("discord.js"); //BibliothÃ¨que Javascript de Discord
 
-const PREFIX = "::";	//Ce qu'il y a au début de la commande, exemple ici : "-:ping"
+const PREFIX = "::";	//Ce qu'il y a au dÃ©but de la commande, exemple ici : "-:ping"
 const PREFIXCOMMAND = "//";
 
-var bot = new Discord.Client(); // C'est pour dire que le bot est comme un utilisateur au serveur (normalement c'est ça, je sais plus, mais c'est obligatoire en gros, cherche pas xD)
+var bot = new Discord.Client(); // C'est pour dire que le bot est comme un utilisateur au serveur (normalement c'est Ã§a, je sais plus, mais c'est obligatoire en gros, cherche pas xD)
 
 
 //ecrit dans la console quand le bot est pret
@@ -15,28 +15,51 @@ bot.on("ready", function () {
 
 
 bot.on("message", function (message) {
-    //ignore son propre message (i guess ? c'est dur d'expliquer)
+    
     if (message.author.equals(bot.user)) return;
 	
 	if (message.content.startsWith(PREFIXCOMMAND)){
 		var args = message.content.substring(PREFIXCOMMAND.length).split(" ");
-
-		switch (args[0].toLowerCase()) {			
+		
+		const emojiList = bot.emojis.map(e=>e.toString()).join("#");
+		var ListEmoji = emojiList.split("#");
+		
+		switch (args[0].toLowerCase()) {		//commands with // prefix	
 			case "random":
-				const emojiList = bot.emojis.map(e=>e.toString()).join("#");
-				var ListEmoji = emojiList.split("#");
+				
 				randemo = Math.floor((Math.random() * ListEmoji.length));
 				message.delete();
 				message.reply(ListEmoji[randemo]);
 				break
-		}
+			
+			case "list":
+				if (!args[1]) currentPage = 0;
+				else currentPage = args[1] - 1; //check if int
+				if (currentPage > pageTot){
+					message.channel.send("Page not found");
+					return;
+				}
+				message.delete();
+				const embed = new Discord.RichEmbed()
+				var limit = 24;
+				if(ListEmoji.length - (25 * currentPage) < 25) limit = ListEmoji.length;
+					for(var i=(24 * currentPage);i<limit; i++){
+						var testemoji = ListEmoji[i];
+						embed.addField(testemoji,
+							ListEmoji[i+1], true)
+						i++
+					}
+				message.channel.send({ embed});
+				message.channel.send("Page " + (currentPage + 1) + " / " + (pageTot + 1));
+				break
+			}
 	}
 	
 
     //Si ne commence pas par le PREFIX, ignore
     if (!message.content.startsWith(PREFIX)) return;
 
-    //sépare les mots de la phrase (ne compte pas le PREFIX)
+    //sÃ©pare les mots de la phrase (ne compte pas le PREFIX)
     var args = message.content.substring(PREFIX.length).split(" ");
 
 	const emoji = bot.emojis.find("name", args[0]);
@@ -48,9 +71,3 @@ bot.on("message", function (message) {
 
 //connecte le bon bot
 bot.login(process.env.BOT_TOKEN)
-
-//const emojiList = bot.emojis.map(e=>e.toString()).join("#");			//message.guild.emojis.map(e=>e.toString()).join("#");
-	//	var ListEmoji = emojiList.split("#");
-	//	for(var i=0;i<ListEmoji.length; i++){
-	//		message.channel.send(ListEmoji[i]);
-	//	}
